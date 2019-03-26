df7a00a2c0daec026c9b88d58a9fde430ac7730f
mandzeete <aivar.loopalu@gmail.com>
2019-03-26 10:03:54 +0200

    Lisatud funktsionaalsust ja stiili (nupud). Lisatud gitignore fail.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,29 +1,177 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.util.Random;
 
 public class Main extends Application {
+    private String currentTime;
+    private String currentFrequency;
 
     @Override
     public void start(Stage stage) throws Exception{
-        Random random = new Random();
+
         Parent root = FXMLLoader.load(getClass().getResource("structure.fxml"));
         root.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
         stage.setTitle("Water Analyzer");
-        Scene scene = new Scene(root, 1400, 800);
+        Scene scene = new Scene(root, 1500, Screen.getPrimary().getVisualBounds().getHeight()*0.9);
         stage.setScene(scene);
         stage.setResizable(false);
+        makeFrequencyButtons(scene);
+        makeTimeButtons(scene);
+        makeChart(scene);
 
+        stage.show();
+    }
+
+    private void makeTimeButtons(Scene scene) {
+        final ToggleButton button1 = new ToggleButton("10 min");
+        final ToggleButton button2 = new ToggleButton("5 min");
+        final ToggleButton button3 = new ToggleButton("3 min");
+        final ToggleButton button4 = new ToggleButton("2 min");
+        final ToggleButton button5 = new ToggleButton("1 min");
+        final ToggleButton button6 = new ToggleButton("30 sec");
+        HBox box1 = new HBox(button1);
+        box1.setId("hbox");
+        HBox box2 = new HBox(button2);
+        box2.setId("hbox");
+        HBox box3 = new HBox(button3);
+        box3.setId("hbox");
+        HBox box4 = new HBox(button4);
+        box4.setId("hbox");
+        HBox box5 = new HBox(button5);
+        box5.setId("hbox");
+        HBox box6 = new HBox(button6);
+        box6.setId("hbox");
+
+        ToggleGroup group = new ToggleGroup();
+        button1.setToggleGroup(group);
+        button2.setToggleGroup(group);
+        button3.setToggleGroup(group);
+        button4.setToggleGroup(group);
+        button5.setToggleGroup(group);
+        button6.setToggleGroup(group);
+        group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle selectedToggle) {
+                if(selectedToggle!=null) {
+                    currentTime = ((ToggleButton) selectedToggle).getText();
+                    System.out.println(currentTime);
+                    //label.setText(((ToggleButton) selectedToggle).getText());
+                }
+                else {
+                    //label.setText("...");
+                }
+            }
+        });
+        // select the first button to start with
+        //group.selectToggle(tb1);
+        // add buttons and label to grid and set their positions
+        GridPane.setConstraints(box1,0,8);
+        GridPane.setConstraints(box2,1,8);
+        GridPane.setConstraints(box3,2,8);
+        GridPane.setConstraints(box4,3,8);
+        GridPane.setConstraints(box5,4,8);
+        GridPane.setConstraints(box6,0,9);
+
+        GridPane grid = (GridPane) scene.lookup("#testSettings");
+
+        grid.getChildren().addAll(box1, box2, box3, box4, box5, box6);
+    }
+
+    private void makeFrequencyButtons(Scene scene) {
+        ToggleButton button1 = new ToggleButton("2 MHz");
+        final ToggleButton button2 = new ToggleButton("1.6 MHz");
+        final ToggleButton button3 = new ToggleButton("1.3 MHz");
+        final ToggleButton button4 = new ToggleButton("1 MHz");
+        final ToggleButton button5 = new ToggleButton("880 kHz");
+        final ToggleButton button6 = new ToggleButton("800 kHz");
+        final ToggleButton button7 = new ToggleButton("660 kHz");
+        final ToggleButton button8 = new ToggleButton("500 kHz");
+        final ToggleButton button9 = new ToggleButton("400 kHz");
+        final ToggleButton button10 = new ToggleButton("300 kHz");
+        HBox box1 = new HBox(button1);
+        box1.setId("hbox");
+        HBox box2 = new HBox(button2);
+        box2.setId("hbox");
+        HBox box3 = new HBox(button3);
+        box3.setId("hbox");
+        HBox box4 = new HBox(button4);
+        box4.setId("hbox");
+        HBox box5 = new HBox(button5);
+        box5.setId("hbox");
+        HBox box6 = new HBox(button6);
+        box6.setId("hbox");
+        HBox box7 = new HBox(button7);
+        box7.setId("hbox");
+        HBox box8 = new HBox(button8);
+        box8.setId("hbox");
+        HBox box9 = new HBox(button9);
+        box9.setId("hbox");
+        HBox box10 = new HBox(button10);
+        box10.setId("hbox");
+
+        ToggleGroup group = new ToggleGroup();
+        button1.setToggleGroup(group);
+        button2.setToggleGroup(group);
+        button3.setToggleGroup(group);
+        button4.setToggleGroup(group);
+        button5.setToggleGroup(group);
+        button6.setToggleGroup(group);
+        button7.setToggleGroup(group);
+        button8.setToggleGroup(group);
+        button9.setToggleGroup(group);
+        button10.setToggleGroup(group);
+        group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle selectedToggle) {
+                if(selectedToggle!=null) {
+                    currentFrequency = ((ToggleButton) selectedToggle).getText();
+                    System.out.println(currentFrequency);
+                    //label.setText(((ToggleButton) selectedToggle).getText());
+                }
+                else {
+                    //label.setText("...");
+                }
+            }
+        });
+        // select the first button to start with
+        //group.selectToggle(tb1);
+        // add buttons and label to grid and set their positions
+        GridPane.setConstraints(box1,0,2);
+        GridPane.setConstraints(box2,1,2);
+        GridPane.setConstraints(box3,2,2);
+        GridPane.setConstraints(box4,3,2);
+        GridPane.setConstraints(box5,4,2);
+        GridPane.setConstraints(box6,0,3);
+        GridPane.setConstraints(box7,1,3);
+        GridPane.setConstraints(box8,2,3);
+        GridPane.setConstraints(box9,3,3);
+        GridPane.setConstraints(box10,4,3);
+
+        GridPane grid = (GridPane) scene.lookup("#testSettings");
+        grid.getChildren().addAll(box1, box2, box3, box4, box5, box6, box7, box8, box9, box10);
+    }
+
+    private void makeChart(Scene scene) {
+        Random random = new Random();
         CategoryAxis xAxis = (CategoryAxis) scene.lookup("#xtelg");
         xAxis.setLabel("Time (s)");
         NumberAxis yAxis = (NumberAxis) scene.lookup("#ytelg");
@@ -35,13 +183,14 @@ public class Main extends Application {
             series.getData().add(new XYChart.Data<>(Integer.toString(i), random.nextInt(4000)+1000));
         }
         LineChart lineChart = (LineChart) scene.lookup("#linechart");
+        lineChart.setLegendVisible(false);
+        lineChart.setCreateSymbols(false);
         lineChart.getData().add(series);
-
-        stage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
